d856f60ae74c66c70cbbac0ffe11ee642e8676e8
Utkarsh Tiwari <uktiwari023@gmail.com>
2024-02-14 14:05:07 +0530

    fixed light theme, added hover effects

--- dist/Scripts/swapColor.js
+++ dist/Scripts/swapColor.js
@@ -1,43 +1,56 @@
-swapbtn = document.getElementById("colorSwapBtn");
+const swapbtn = document.getElementById("colorSwapBtn");
+const border = document.querySelectorAll("#border");
 setColors(2);
 swapbtn.innerHTML = `<i class='bx bxs-sun'></i>`;
 let isColorSet1 = false;
 
 swapbtn.addEventListener("click", function () {
- if (isColorSet1) {
-  swapbtn.innerHTML = `<i class='bx bxs-sun'></i>`;
-  setColors(2);
-  document.body.style.color = "#fff";
-  isColorSet1 = false;
- } else {
-  swapbtn.innerHTML = `<i class='bx bxs-moon'></i>`;
-  setColors(1);
-  document.body.style.color = "#000";
-  isColorSet1 = true;
- }
+    if (isColorSet1) {
+        swapbtn.innerHTML = `<i class='bx bxs-sun'></i>`;
+        setColors(2);
+        document.body.style.color = "#fff";
+        // document.body.style.fontWeight = "400";
+
+        border.forEach(function (i) {
+            i.style.border = '2px solid #fff';
+        });
+        
+        isColorSet1 = false;
+    } 
+    
+    else {
+        swapbtn.innerHTML = `<i class='bx bxs-moon'></i>`;
+        setColors(1);
+        document.body.style.color = "#000";
+        // document.body.style.fontWeight = "600";
+        border.forEach(function (i) {
+            i.style.border = '2px solid #000';
+        });
+        isColorSet1 = true;
+    }
 });
 
 function setColors(setNumber) {
- // Set the colors based on the setNumber
- const primaryColor = getComputedStyle(
-  document.documentElement
- ).getPropertyValue(`--primary-color-${setNumber}`);
- const secondaryColor = getComputedStyle(
-  document.documentElement
- ).getPropertyValue(`--secondary-color-${setNumber}`);
- const accentColor = getComputedStyle(
-  document.documentElement
- ).getPropertyValue(`--accent-color-${setNumber}`);
- const fontColor = getComputedStyle(document.documentElement).getPropertyValue(
-  `--font-color-${setNumber}`
- );
+    // Set the colors based on the setNumber
+    const primaryColor = getComputedStyle(
+        document.documentElement
+    ).getPropertyValue(`--primary-color-${setNumber}`);
+    const secondaryColor = getComputedStyle(
+        document.documentElement
+    ).getPropertyValue(`--secondary-color-${setNumber}`);
+    const accentColor = getComputedStyle(
+        document.documentElement
+    ).getPropertyValue(`--accent-color-${setNumber}`);
+    const fontColor = getComputedStyle(document.documentElement).getPropertyValue(
+        `--font-color-${setNumber}`
+    );
 
- // Update the root color variables
- document.documentElement.style.setProperty("--primary-color", primaryColor);
- document.documentElement.style.setProperty(
-  "--secondary-color",
-  secondaryColor
- );
- document.documentElement.style.setProperty("--accent-color", accentColor);
- document.documentElement.style.setProperty("--font-color", fontColor);
+    // Update the root color variables
+    document.documentElement.style.setProperty("--primary-color", primaryColor);
+    document.documentElement.style.setProperty(
+        "--secondary-color",
+        secondaryColor
+    );
+    document.documentElement.style.setProperty("--accent-color", accentColor);
+    document.documentElement.style.setProperty("--font-color", fontColor);
 }
