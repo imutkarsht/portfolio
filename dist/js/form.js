284639daa11aa1f6601aad17518e4d90a67a08ba
Utkarsh Tiwari <uktiwari023@gmail.com>
2024-02-19 17:49:45 +0530

    Improved Submit form JS

--- dist/js/form.js
+++ dist/js/form.js
@@ -1,11 +1,32 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbw50fI3eEMVLWU60e3_vXsFYqTJChXDJInMcgwhNHDSBpP7dMPhYpNAVtYy7bxbhxd3cw/exec'
-
-const form = document.forms['subform']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw50fI3eEMVLWU60e3_vXsFYqTJChXDJInMcgwhNHDSBpP7dMPhYpNAVtYy7bxbhxd3cw/exec';
+const form = document.forms['subform'];
+const submitButton = form.querySelector('button[type="submit"]');
+const btn = document.querySelector('.submit');
+const messageBox = document.querySelector('.msgBox');
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your response is recorded successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+  e.preventDefault(); 
+  submitButton.textContent = 'Please Wait...'; 
+  btn.classList.add('submitting');
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      if (response.ok) {
+        messageBox.innerHTML = `Thank you! Your Response was recorded Successfully...`;
+        submitButton.textContent = 'Submit'; 
+        btn.classList.remove('submitting');
+        setTimeout(() => {
+          messageBox.innerHTML = '';
+        }, 3000); 
+        return response.json();
+      } else {
+        throw new Error('Network response was not ok.');
+      }
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      submitButton.textContent = 'Submit'; 
+    });
+});
